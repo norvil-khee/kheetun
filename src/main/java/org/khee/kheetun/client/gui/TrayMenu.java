340b7a92fe3983815857c842d9adcd58f915ed37
Volker Arend <vohe@if-sat.de>
2016-11-15 10:10:23 +0100

    restructured to single project

--- src/main/java/org/khee/kheetun/client/gui/TrayMenu.java
+++ src/main/java/org/khee/kheetun/client/gui/TrayMenu.java
@@ -3,7 +3,6 @@ package org.khee.kheetun.client.gui;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
-import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Insets;
